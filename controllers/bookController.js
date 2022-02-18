0658b75a351b0950736924e09b3cb07c06926cfa
ll3m <ll3m@fresnou.org>
2022-02-18 09:42:41 -0800

    slide 19 of 5.3 pt1

---
+++ controllers/bookController.js
@@ -0,0 +1,19 @@
+const {Book} = require('../models');
+
+//view all
+module.exports.viewAll = async function(req,res){
+    const books = book.findAll();
+    res.render('book/view_all', {books});
+};
+
+//profile
+
+//render add form
+
+//add
+
+//render edit form
+
+//update
+
+//delete
